a6ba1c55a72e761059be778894cb5b182357929f
RESTY3 <restynassaka3@gmail.com>
2022-06-10 14:39:10 -0700

    portfolio

---
+++ JS-BASICS/index.js
@@ -0,0 +1,29 @@
+//console.log('Hello World');
+//let name ='Resty';
+//let age = 30;
+//let isApproved = true;
+
+// arrays
+//let selectedColors=['red','blue'];
+//console.log(selectedColors);
+
+// function greet (name, lastName) {
+
+//     name = 'Bk'
+//     lastName = 'bg'
+//     console.log('Hello' + name + '' + lastName);
+// }
+
+//greet('John', 'Smith');
+//function displayName(name , last){
+    //console.log("Hello " + name + ' ' + last);
+//}
+
+//displayName("Bk", "bg");
+
+function square(number) {
+    return number*number;
+}
+
+let number = square(4);
+console.log(number);
